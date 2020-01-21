55feecdf99bb24ca3a99dbe5a017477bb0dc1790
mikaelaalu <54809602+mikaelaalu@users.noreply.github.com>
2020-01-21 11:53:01 +0100

    added a lot of npm install

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,25 @@ const browsersync = require("rollup-plugin-browsersync");
 // ES6
 import { terser } from "rollup-plugin-terser";
 
+// rollup.config.js
+import postcss from "rollup-plugin-postcss";
+import cssnano from "cssnano";
+
+// const postcss = require("postcss");
+const postcssNormalize = require("postcss-normalize");
+
+const autoprefixer = require("autoprefixer");
+
+// new way but i do it old way
+
+// export default {
+//   plugins: [
+//     postcss({
+//       plugins: []
+//     })
+//   ]
+// };
+
 const isProduction = process.env.NODE_ENV === "production";
 const isDevelopment = isProduction === false;
 
@@ -16,13 +35,21 @@ module.exports = {
 
   plugins: [
     isDevelopment && browsersync({ server: "public" }),
-    isProduction && terser()
+    isProduction && terser(),
+    postcss({
+      extract: true,
+      plugins: [
+        postcssNormalize(/* pluginOptions */),
+        autoprefixer(),
+        cssnano()
+      ],
+      sourceMap: isDevelopment
+    })
   ]
 };
-
-// ES6
+// // ES6
 // import browsersync from 'rollup-plugin-browsersync'
-//
+
 // export default {
 //   input: 'src/scripts/index.js',
 //   output: {
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,2 @@
+import "../styles/index.scss";
 console.log("Hello Rollup, this is a change");
